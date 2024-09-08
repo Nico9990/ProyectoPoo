67531879a97b8a8e1af63b506bfc17039f2ca9d1
Nico9990 <nicolasguzman070@gmail.com>
2024-09-08 14:12:50 -0600

    Inicie el trabajo

---
+++ contraseña.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class Main {
+  public static void main(String[] args) {
+    Scanner entrada = new Scanner(System.in);
+    int x = 1234; 
+            int g;
+            int intentos = 0;
