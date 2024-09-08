2492b46da8daefbe5e64d87a8bd90794e310a09d
Amet Perez <ametitoop52@gmail.com>
2024-09-08 14:28:06 -0600

    Tarea Finalizada

--- contraseña.java
+++ contraseña.java
@@ -5,3 +5,22 @@ public class Main {
     int x = 1234; 
             int g;
             int intentos = 0;
+
+            System.out.println("Ingresa la contraseña");
+
+            do {
+                g = entrada.nextInt();
+                intentos++;
+            } while (g != x && intentos < 4);
+
+            if (intentos == 4) {
+                System.out.println("Limite de intentos alcanzado");
+            } else {
+                System.out.println("La contraseña es correcta");
+            }
+        }
+    }
+
+        
+      
+  
